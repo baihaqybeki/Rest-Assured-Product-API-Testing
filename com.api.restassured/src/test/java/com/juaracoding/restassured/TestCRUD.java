3e0bd94338188a2ae879a7f8e1a66e4d72c4d244
Bestada <Bestada@DESKTOP-VV8P387>
2022-08-25 11:15:09 +0700

    Second Commit

--- com.api.restassured/src/test/java/com/juaracoding/restassured/TestCRUD.java
+++ com.api.restassured/src/test/java/com/juaracoding/restassured/TestCRUD.java
@@ -27,6 +27,54 @@ public class TestCRUD {
 		.then()
 		.statusCode(200)
 		.log().all();
-
+	}
+	
+	@Test
+	public void testPut() {
+		JSONObject request = new JSONObject();
+		request.put("name", "Laptop 2nd");
+		request.put("category", "Elektronik");
+		request.put("price", "7500");
+		System.out.println(request.toJSONString());
+		
+		given()
+		.header("content-type","application/json")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+		.body(request.toJSONString())
+		.when()
+		.patch("https://mern-backend-8881.herokuapp.com/products/6305a40bd513f87e93c28f87")
+		.then()
+		.statusCode(200)
+		.log().all();
+	}
+	
+	@Test
+	public void testPatch() {
+		JSONObject request = new JSONObject();
+		request.put("name", "Laptop Second");
+		request.put("category", "Elektronik");
+		request.put("price", "5000");
+		System.out.println(request.toJSONString());
+		
+		given()
+		.header("content-type","application/json")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+		.body(request.toJSONString())
+		.when()
+		.patch("https://mern-backend-8881.herokuapp.com/products/6305a40bd513f87e93c28f87")
+		.then()
+		.statusCode(200)
+		.log().all();
+	}
+	
+	@Test
+	public void testDelete() {
+		when()
+		.delete("https://mern-backend-8881.herokuapp.com/products/6305a40bd513f87e93c28f87")
+		.then()
+		.statusCode(200)
+		.log().all();
 	}
 }
